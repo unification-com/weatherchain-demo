b00f16fe8a031960d4ac38d38de4f44b49f9512e
Codegnosis <paul@codegnosis.co.uk>
2019-02-28 11:11:05 +0000

    weather service - reduce Docker layers

--- Docker/assets/scripts/deploy_weather_service.sh
+++ Docker/assets/scripts/deploy_weather_service.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+cp /root/build/.env /root/weather-oracle-contract/.env
 cd /root/weather-oracle-contract
-truffle migrate --reset 2>&1 | tee sc_log.txt
+truffle compile && truffle migrate --reset 2>&1 | tee sc_log.txt
 
-WORKCHAIN_NETWORK_ID_ENV=$(grep 'WORKCHAIN_NETWORK_ID' .env)
+WORKCHAIN_NETWORK_ID_ENV=$(grep 'WORKCHAIN_NETWORK_ID' /root/build/.env)
 WORKCHAIN_NETWORK_ID=${WORKCHAIN_NETWORK_ID_ENV##*=}
 
-sed -i "s/WEATHER_ORACLE_CONTRACT_ADDRESS=/WEATHER_ORACLE_CONTRACT_ADDRESS=$(node abi.js addr ${WORKCHAIN_NETWORK_ID})/g" /root/weather-oracle-contract/.env
-sed -i "s/WEATHER_ORACLE_ABI=/WEATHER_ORACLE_ABI=$(node abi.js)/g" /root/weather-oracle-contract/.env
-sed -i "s/WEATHER_ORACLE_CONTRACT_ADDRESS=/WEATHER_ORACLE_CONTRACT_ADDRESS=$(node abi.js addr ${WORKCHAIN_NETWORK_ID})/g" /root/weather-oracle-service/.env
-sed -i "s/WEATHER_ORACLE_ABI=/WEATHER_ORACLE_ABI=$(node abi.js)/g" /root/weather-oracle-service/.env
-sed -i "s/WEATHER_ORACLE_CONTRACT_ADDRESS=/WEATHER_ORACLE_CONTRACT_ADDRESS=$(node abi.js addr ${WORKCHAIN_NETWORK_ID})/g" /root/weather-ui/.env
-sed -i "s/WEATHER_ORACLE_ABI=/WEATHER_ORACLE_ABI=$(node abi.js)/g" /root/weather-ui/.env
+sed -i "s/WEATHER_ORACLE_CONTRACT_ADDRESS=/WEATHER_ORACLE_CONTRACT_ADDRESS=$(node abi.js addr ${WORKCHAIN_NETWORK_ID})/g" /root/build/.env
+sed -i "s/WEATHER_ORACLE_ABI=/WEATHER_ORACLE_ABI=$(node abi.js)/g" /root/build/.env
 
-sleep 1s
+cp /root/build/.env /root/weather-oracle-service/.env
+cp /root/build/.env /root/weather-ui/.env
 
 cd /root/weather-oracle-service && npm run develop 2>&1 | tee service_log.txt & cd /root/weather-ui && node server.js
